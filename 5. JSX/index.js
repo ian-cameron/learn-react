7ca9c4934d167d907e8b37fed18845ad97f32233
ian-cameron <ian@digiwest.com>
2022-07-29 16:32:00 -0700

    Add lesson 5

---
+++ 5. JSX/index.js
@@ -0,0 +1,33 @@
+// JSX
+ReactDOM.render(<h1 className="heading">This is JSX</h1>, document.getElementById("root"));
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+const navbarElement = (
+    <nav>
+        <h1 className="brand">Learn React</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbarElement, document.getElementById("root"));
